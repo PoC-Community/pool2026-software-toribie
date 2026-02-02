7fb074d4863b73054142a70ef5ae5e7bfa4c3ec2
Olivier Tran <olivier.tran@epitech.eu>
2026-02-02 11:48:38 +0100

    feat: some amount of step1

---
+++ src/day1/script.js
@@ -0,0 +1,4 @@
+document.getElementById('click-me').addEventListener('click', () => {
+    console.log('Button clicked!');
+    alert("You haven't noticed it yet, but the cookie is already in your stomach.");
+});
