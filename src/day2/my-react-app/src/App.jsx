0ad138624f4be45f8100da60c310872c97a1178a
Olivier Tran <olivier.tran@epitech.eu>
2026-02-03 14:57:36 +0100

    feat: step 2.2

--- src/day2/my-react-app/src/App.jsx
+++ src/day2/my-react-app/src/App.jsx
@@ -9,12 +9,12 @@ function MyHeader() {
     );
 }
 
-function MyButton(props) {
-  return (
-    <button>
-      You have eaten {props.foodcount} {props.food} today!
-    </button>
-  );
+function CountState(props) {
+    return (
+      <h4>
+        You have eaten {props.foodcount} {props.food} today! You must be quite hungry are ya? (¯▿¯)
+      </h4>
+    );
 }
 
 function MyCard(props) {
@@ -26,30 +26,67 @@ function MyCard(props) {
   );
 }
 
+function CountButton(props) {
+  return (
+  <button onClick={props.onClick}>
+    {props.description}
+  </button>
+  );
+}
+
+function MyForm() {
+  const [name, setName] = useState("");
+
+  return (
+    <form>
+      <label>Enter your name:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <p>Current value: {name}</p>
+    </form>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  function handleClick(increment) {
+    if (increment == 0) {
+      setCount(0);
+    } else {
+      setCount(count + increment);
+    }
+  }
 
   return (
     <>
     <MyHeader />
     <div>
       <h2>Welcome to my app</h2>
-      <MyButton food="carrots" foodcount = {count} onClick={() => setCount((count) => count + 1)}/>
     </div>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          click to eat
-        </button>
         <br></br>
         <br></br>
+        <CountState food="carrots" foodcount={count}/>
+        <div className="countbuttons">
+        <CountButton description="Click to eat!" onClick={() => handleClick(1)}/>
+        <CountButton description="Click to clinically replace your stomach by a clean one via surgery!" onClick={() => handleClick(0)}/>
+        <CountButton description="Click to digest!" onClick={() => handleClick(-1)}/>
+        </div>
         <MyCard title="Example Card" description="Hello World"/>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        <a href="https://vite.dev" target="_blank">Vite</a> & <a href="https://react.dev" target="_blank">React</a> docs.
-      </p>
+      <footer>
+        <p className="read-the-docs">
+          <a href="https://vite.dev" target="_blank">Vite</a> & <a href="https://react.dev" target="_blank">React</a> docs.
+        </p>
+      </footer>
     </>
   )
 }
